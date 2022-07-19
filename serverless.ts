5da45f23583feefd4ab1c592a8846ee0e0e69385
Pasindu Dilshan <pasindu_dilshan@outlook.com>
2022-07-19 22:41:56 +0530

    Initial commit

---
+++ serverless.ts
@@ -0,0 +1,32 @@
+import type { AWS } from "@serverless/typescript";
+
+import hello from "@functions/hello";
+
+const serverlessConfiguration: AWS = {
+    service: "aws-nodejs-typescript",
+    frameworkVersion: "3",
+    plugins: ["serverless-webpack"],
+    provider: {
+        name: "aws",
+        runtime: "nodejs14.x",
+        apiGateway: {
+            minimumCompressionSize: 1024,
+            shouldStartNameWithService: true
+        },
+        environment: {
+            AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+            NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000"
+        }
+    },
+    // import the function via paths
+    functions: { hello },
+    package: { individually: true },
+    custom: {
+        webpack: {
+            webpackConfig: "webpack.config.js",
+            includeModules: true
+        }
+    }
+};
+
+module.exports = serverlessConfiguration;
